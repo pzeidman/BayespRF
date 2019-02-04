9350c3997d287be2c6c8690411325c77afdc2957
MSchnei <marian.schneider@maastrichtuniversity.nl>
2019-02-04 14:36:08 +0100

    Set image scale for stimulus and pRF model representation as optional arg. - Previously, this was set as 41 automatically inside the script. - The default value still is 41 if not provided as input argument.
    Conflicts:
    	example_3T/scripts/prepare_inputs_polar_samsrf.m

--- example_3T/scripts/prepare_inputs_polar_samsrf.m
+++ example_3T/scripts/prepare_inputs_polar_samsrf.m
@@ -1,12 +1,17 @@
-function U = prepare_inputs_polar_samsrf(ApFrm,TR)
+function U = prepare_inputs_polar_samsrf(ApFrm,TR, nmicrotime, stim_duration, stim_diameter, varargin)
 % Produces the input structure needed for spm_prf_analyse() given a 3D
 % stimuli matrix with dimensions [x,y,t].
 %
 % Inputs:
 %
-% ApFrm       - [x,y,t] binary matrix indiciating which pixel locations
-%               were illuminated at each time point t
-% TR          - Scanner repetition time (TR)
+% ApFrm         - [x,y,t] binary matrix indiciating which pixel locations
+%                 were illuminated at each time point t
+% TR            - Scanner repetition time (TR)
+% nmicrotime    - Bins per TR
+% stim_duration - Duration of stimuli (secs)
+% stim_diameter - Diameter of stimuli in degrees
+% imscale       - (optional) Scale of image space for modelling, default 41
+
 %
 % Returns:
 %
@@ -19,34 +24,51 @@ stim_diameter = 17;     % Diameter of stimuli in degrees
 
 n = size(ApFrm,3);
 
+% Cechk for optional inputs
+numvarargs = length(varargin);
+if numvarargs > 1
+    error('varargin:TooManyInputs', ...
+          'requires at most 1 optional inputs');
+end
+
+% set defaults for optional inputs
+optargs = {41};
+
+% now put these defaults into the valuesToUse cell array,
+% and overwrite the ones specified in varargin.
+optargs(1:numvarargs) = varargin;
+
+% Place optional args in memorable variable names
+[imscale] = optargs{:};
+
 for t = 1:n
-    
+
     % Read
     im = ApFrm(:,:,t);
-    
+
     % Skip if a baseline volume
     if ~any(im)
         continue;
     end
-    
-    % Rescale to 41 x 41 resolution
-    res = [41 41];
+
+    % Rescale to imscale x imscale resolution
+    res = [imscale imscale];
     im  = imresize(im,res);
-    
+
     % Binarize
     im = im > 0.01;
-    
+
     % Extract stimulated coordinates
     [y,x] = ind2sub(res,find(im));
-    
-    % Rescale [1,41] to to units of visual angle (degrees)
+
+    % Rescale [1,imscale] to units of visual angle (degrees)
     r = (stim_diameter/2);
     x = rescale(x, 1, res(1), -r, r);
     y = rescale(y, 1, res(1), -r, r);
-    
+
     % Flip the y axis so positive is up
     y = -y;
-            
+
     % Convert from x,y to distance >= 0 and angle (-pi,pi]
     dist  = sqrt( (x .^ 2) + (y .^ 2) );
     angle = atan2(y,x);
@@ -63,7 +85,7 @@ for t = 1:n
 end
 
 % -------------------------------------------------------------------------
-function new_X = rescale(X, current_min, current_max, new_min, new_max)   
+function new_X = rescale(X, current_min, current_max, new_min, new_max)
 % Rescale X to the range [new_min new_max]
 scale_factor = (current_max - current_min) / (new_max - new_min);
-new_X = new_min + (X - current_min) / scale_factor;    
+new_X = new_min + (X - current_min) / scale_factor;
